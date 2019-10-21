e00e938daec14c005275bf8c1f33a04460ea4adb
Michel Engelen <michel@nomorejs.net>
2019-10-21 18:00:24 +0200

    added more settings (language needs redux stuff though) :D

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -5,6 +5,8 @@ import 'package:sps/components/wrapper.dart';
 import 'package:sps/constants/keys.dart';
 import 'package:sps/models/app_state.dart';
 
+enum WhyFarther { harder, smarter, selfStarter, tradingCharter }
+
 class Settings extends StatelessWidget {
   final void Function() toggleTheme;
 
@@ -19,12 +21,47 @@ class Settings extends StatelessWidget {
         print(settings);
         return new Wrapper(
           title: 'Settings',
-          child: new Center(
-            child: new Switch(
-              value: darkMode,
-              onChanged: (bool darkMode) { toggleTheme(); },
-            ),
-          ),
+          child: Container(
+              padding: EdgeInsets.symmetric(vertical: 24),
+              child: ListView(
+                children: ListTile.divideTiles(context: context, tiles: [
+                  SwitchListTile(
+                    title: Text('Enable Dark Mode'),
+                    value: darkMode,
+                    onChanged: (bool value) {
+                      toggleTheme();
+                    },
+                    secondary: Icon(Icons.brightness_3),
+                  ),
+                  ListTile(
+                    title: Text('Language'),
+                    leading: Icon(Icons.flag),
+                    // leading: Text('EN'),
+                    trailing: PopupMenuButton<WhyFarther>(
+                      onSelected: (WhyFarther result) {},
+                      itemBuilder: (BuildContext context) =>
+                          <PopupMenuEntry<WhyFarther>>[
+                        const PopupMenuItem<WhyFarther>(
+                          value: WhyFarther.harder,
+                          child: Text('Working a lot harder'),
+                        ),
+                        const PopupMenuItem<WhyFarther>(
+                          value: WhyFarther.smarter,
+                          child: Text('Being a lot smarter'),
+                        ),
+                        const PopupMenuItem<WhyFarther>(
+                          value: WhyFarther.selfStarter,
+                          child: Text('Being a self-starter'),
+                        ),
+                        const PopupMenuItem<WhyFarther>(
+                          value: WhyFarther.tradingCharter,
+                          child: Text('Placed in charge of trading charter'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ]).toList(),
+              )),
         );
       },
     );
