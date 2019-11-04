d3bfc4903340fc56d8e487e90a228c4c1db4eecc
Michel Engelen <michel@nomorejs.net>
2019-11-04 11:43:08 +0100

    made snackbars dismissable by default

--- lib/utils/snackbar.dart
+++ lib/utils/snackbar.dart
@@ -4,10 +4,26 @@ import 'package:flutter/material.dart';
 
 Completer<void> snackBarCompleter(
   BuildContext context,
-  Object message,
-  { bool shouldPop = false }
-) {
+  Object message, {
+  bool shouldPop = false,
+  bool dismissable = true,
+  Function snackbarAction,
+  String snackbarActionContent,
+}) {
   final Completer<void> completer = Completer<void>();
+  dynamic action;
+
+  if (dismissable) {
+    action = SnackBarAction(
+      label: 'X',
+      onPressed: Scaffold.of(context).hideCurrentSnackBar,
+    );
+  } else if (snackbarAction is Function) {
+    action = SnackBarAction(
+      label: snackbarActionContent ?? 'X',
+      onPressed: snackbarAction,
+    );
+  }
 
   completer.future.then((_) {
     if (shouldPop) {
@@ -16,6 +32,7 @@ Completer<void> snackBarCompleter(
     Scaffold.of(context).hideCurrentSnackBar();
     Scaffold.of(context).showSnackBar(SnackBar(
       content: Text(message),
+      action: action,
     ));
   }).catchError((Object error) {
     print('#### Error: $error');
